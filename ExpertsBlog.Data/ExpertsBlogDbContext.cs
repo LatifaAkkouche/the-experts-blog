6659b92d0fba6c5764ebc9d87013da203c6bdd9a
Andrés Talavera <andres.talavera@live.fr>
2022-02-04 01:26:40 +0100

    Create ExpertsBlogDbContext.cs

---
+++ ExpertsBlog.Data/ExpertsBlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExpertsBlog.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpertsBlog.Data
+{
+    public class ExpertsBlogDbContext : DbContext
+    {
+        DbSet<BlogPost> BlogPosts { get; set; }
+
+        public ExpertsBlogDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+    }
+}
